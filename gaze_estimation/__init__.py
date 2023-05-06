d491a3db68f117cf1fca1d6fbb386372bfd39d4f
Zagreus98 <alex.ombla@gmail.com>
2023-05-06 20:17:15 +0300

    updated

--- gaze_estimation/__init__.py
+++ gaze_estimation/__init__.py
@@ -1,10 +1,10 @@
 from .config import get_default_config
 from .dataloader import create_dataloader
 from .logger import create_logger
-from .losses import create_loss
-from .models import create_model
+from .losses import TotalLoss
 from .optim import create_optimizer
 from .scheduler import create_scheduler
 from .tensorboard import create_tensorboard_writer
 from .transforms import create_transform
 from .types import GazeEstimationMethod, LossType
+from .engine import create_model

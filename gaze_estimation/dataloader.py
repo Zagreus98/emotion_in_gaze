a09df8dcdcf0bc519b0dab460a63fe76b73b65fa
Zagreus98 <alex.ombla@gmail.com>
2023-05-06 20:14:14 +0300

    integrated ethxgaze and rafdb datasets

--- gaze_estimation/dataloader.py
+++ gaze_estimation/dataloader.py
@@ -11,21 +11,19 @@ def create_dataloader(
         is_train: bool) -> Union[Tuple[DataLoader, DataLoader], DataLoader]:
     if is_train:
         train_dataset, val_dataset = create_dataset(config, is_train)
+        # TODO: create weights for each dataset
         train_loader = DataLoader(
             train_dataset,
             batch_size=config.train.batch_size,
             shuffle=True,
             num_workers=config.train.train_dataloader.num_workers,
-            pin_memory=config.train.train_dataloader.pin_memory,
-            drop_last=config.train.train_dataloader.drop_last,
+
         )
         val_loader = DataLoader(
             val_dataset,
             batch_size=config.train.batch_size,
             shuffle=False,
             num_workers=config.train.val_dataloader.num_workers,
-            pin_memory=config.train.val_dataloader.pin_memory,
-            drop_last=False,
         )
         return train_loader, val_loader
     else:

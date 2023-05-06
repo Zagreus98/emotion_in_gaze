cf39c64be74527c511192a6e1c40ff2cca927e14
Zagreus98 <alex.ombla@gmail.com>
2023-05-06 20:16:50 +0300

    added transforms for each type of dataset

--- gaze_estimation/transforms.py
+++ gaze_estimation/transforms.py
@@ -4,28 +4,68 @@ import cv2
 import numpy as np
 import torch
 import torchvision
+from torchvision import transforms as T
 import yacs.config
 
 from .types import GazeEstimationMethod
 
 
 def create_transform(config: yacs.config.CfgNode) -> Any:
-    if config.mode == GazeEstimationMethod.MPIIGaze.name:
-        return _create_mpiigaze_transform(config)
-    elif config.mode == GazeEstimationMethod.MPIIFaceGaze.name:
+    if config.mode == GazeEstimationMethod.MPIIFaceGaze.name:
         return _create_mpiifacegaze_transform(config)
     else:
         raise ValueError
 
 
-def _create_mpiigaze_transform(config: yacs.config.CfgNode) -> Any:
-    scale = torchvision.transforms.Lambda(lambda x: x.astype(np.float32) / 255)
-    transform = torchvision.transforms.Compose([
-        scale,
-        torch.from_numpy,
-        torchvision.transforms.Lambda(lambda x: x[None, :, :]),
+def create_xgaze_transform(config: yacs.config.CfgNode, stage: str) -> Any:
+    assert stage in ['train', 'val', 'test']
+    mean = np.array(config.dataset.mean)
+    std = np.array(config.dataset.std)
+    transform_config = getattr(config.dataset.transform, stage)
+
+    transforms = [
+        T.ToPILImage(),
+    ]
+    if ('resize' in transform_config
+            and transform_config.resize != config.dataset.image_size):
+        transforms.append(T.Resize(transform_config.resize))
+    if 'color_jitter' in transform_config:
+        transforms.append(
+            T.ColorJitter(brightness=transform_config.color_jitter.brightness,
+                          contrast=transform_config.color_jitter.contrast,
+                          saturation=transform_config.color_jitter.saturation,
+                          hue=transform_config.color_jitter.hue))
+
+    transforms.extend([
+        T.ToTensor(),
+        T.Normalize(mean, std, inplace=False),
     ])
-    return transform
+
+    return T.Compose(transforms)
+
+
+def create_rafdb_tranform(config: yacs.config.CfgNode, stage: str) -> Any:
+    assert stage in ['train', 'val', 'test']
+    mean = np.array(config.dataset.mean)
+    std = np.array(config.dataset.std)
+    transform_config = getattr(config.dataset.transform, stage)
+
+    if stage == 'train':
+        transforms = T.Compose([
+            T.RandomHorizontalFlip(),
+            T.Resize(transform_config.resize),
+            T.ToTensor(),
+            T.Normalize(mean, std, inplace=False),
+            T.RandomErasing(scale=(0.02, 0.25))
+        ])
+    else:
+        transforms = T.Compose([
+            T.Resize(transform_config.resize),
+            T.ToTensor(),
+            T.Normalize(mean, std, inplace=False),
+        ])
+
+    return transforms
 
 
 def _create_mpiifacegaze_transform(config: yacs.config.CfgNode) -> Any:

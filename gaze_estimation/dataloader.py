d29a689c26feb8c02bda5e7e8edfb8254cb9f51f
Zagreus98 <alex.ombla@gmail.com>
2023-05-07 22:56:34 +0300

    removed mpiiface dataset

--- gaze_estimation/dataloader.py
+++ gaze_estimation/dataloader.py
@@ -1,4 +1,4 @@
-from typing import Tuple, Union
+from typing import Tuple
 
 import yacs.config
 from torch.utils.data import DataLoader, WeightedRandomSampler
@@ -8,37 +8,25 @@ from .datasets import create_dataset
 
 def create_dataloader(
         config: yacs.config.CfgNode,
-        is_train: bool) -> Union[Tuple[DataLoader, DataLoader], DataLoader]:
-    if is_train:
-        train_dataset, val_dataset, sampler_weights = create_dataset(config, is_train)
-        data_sampler = WeightedRandomSampler(
-            weights=sampler_weights,
-            num_samples=len(train_dataset),
-            replacement=True,
-        )
-        # TODO: create weights for each dataset
-        train_loader = DataLoader(
-            train_dataset,
-            batch_size=config.train.batch_size,
-            num_workers=config.train.train_dataloader.num_workers,
-            sampler=data_sampler,
+) -> Tuple[DataLoader, DataLoader]:
 
-        )
-        val_loader = DataLoader(
-            val_dataset,
-            batch_size=config.train.batch_size,
-            shuffle=False,
-            num_workers=config.train.val_dataloader.num_workers,
-        )
-        return train_loader, val_loader
-    else:
-        test_dataset = create_dataset(config, is_train)
-        test_loader = DataLoader(
-            test_dataset,
-            batch_size=config.test.batch_size,
-            num_workers=config.test.dataloader.num_workers,
-            shuffle=False,
-            pin_memory=config.test.dataloader.pin_memory,
-            drop_last=False,
-        )
-        return test_loader
+    train_dataset, val_dataset, sampler_weights = create_dataset(config)
+    data_sampler = WeightedRandomSampler(
+        weights=sampler_weights,
+        num_samples=len(train_dataset),
+        replacement=True,
+    )
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=config.train.batch_size,
+        num_workers=config.train.train_dataloader.num_workers,
+        sampler=data_sampler,
+
+    )
+    val_loader = DataLoader(
+        val_dataset,
+        batch_size=config.train.batch_size,
+        shuffle=False,
+        num_workers=config.train.val_dataloader.num_workers,
+    )
+    return train_loader, val_loader
--- gaze_estimation/datasets/__init__.py
+++ gaze_estimation/datasets/__init__.py
@@ -1,141 +1,89 @@
 import pathlib
 from typing import List, Union, Tuple
 
-import torch
 import json
 import yacs.config
 from torch.utils.data import Dataset, ConcatDataset
 
-from ..transforms import create_transform, create_xgaze_transform, create_rafdb_tranform
-from ..types import GazeEstimationMethod
+from ..transforms import create_xgaze_transform, create_rafdb_tranform
 from .rafdb import RafDataset
+from .ethxgaze import OnePersonDataset
 
 
 def create_dataset(
         config: yacs.config.CfgNode,
-        is_train: bool = True,
 ) -> Union[List[Dataset], Dataset, Tuple[ConcatDataset, ConcatDataset, List]]:
-    # TODO: remove all MPIIFaceGaze mentions and if elses
-    if config.mode == GazeEstimationMethod.MPIIFaceGaze.name:
-        from .mpiifacegaze import OnePersonDataset
-    elif config.mode == GazeEstimationMethod.ETHXGaze.name:
-        from .ethxgaze import OnePersonDataset
-    else:
-        raise ValueError
 
-    # Initialize paths
+    # Initialize dataset paths
     dataset_dir = pathlib.Path(config.dataset.dataset_dir)
     raf_dir = pathlib.Path(config.dataset.raf_dataset_path)
     assert dataset_dir.exists()
     assert raf_dir.exists()
 
-    if config.mode == GazeEstimationMethod.ETHXGaze.name:
-        train_dataset = []
-        val_dataset = []
-        sampler_weights = []
+    train_dataset = []
+    val_dataset = []
+    sampler_weights = []
 
-        # ETH-XGaze datatasets reading
-        split_file = dataset_dir / 'train_test_split.json'
-        with open(split_file) as f:
-            split = json.load(f)
-        train_paths = [
-            dataset_dir / 'train' / name for name in split['train']
-        ]
-        for path in train_paths:
-            assert path.exists()
+    # ETH-XGaze datasets reading
+    split_file = dataset_dir / 'train_test_split.json'
+    with open(split_file) as f:
+        split = json.load(f)
+    train_paths = [
+        dataset_dir / 'train' / name for name in split['train']
+    ]
+    for path in train_paths:
+        assert path.exists()
 
-        # Define gaze transforms
-        gaze_train_transform = create_xgaze_transform(config, 'train')
-        gaze_val_transform = create_xgaze_transform(config, 'val')
-        # Define emotion transforms
-        emo_train_transfrom = create_rafdb_tranform(config, 'train')
-        emo_val_transfrom = create_rafdb_tranform(config, 'train')
-        val_indices = config.train.val_indices
-        assert val_indices is not None
+    # Define gaze transforms
+    gaze_train_transform = create_xgaze_transform(config, 'train')
+    gaze_val_transform = create_xgaze_transform(config, 'val')
+    # Define emotion transforms
+    emo_train_transfrom = create_rafdb_tranform(config, 'train')
+    emo_val_transfrom = create_rafdb_tranform(config, 'val')
+    # Get val indices for person datasets on which to perform evaluation
+    val_indices = config.train.val_indices
+    assert val_indices is not None
 
-        # Prepare train dataset
-        gaze_train_datasets = [
-            OnePersonDataset(path, gaze_train_transform)
-            for i, path in enumerate(train_paths)
-            if i not in val_indices
-        ]
-        emotion_train_dataset = RafDataset(
-            raf_path=raf_dir,
-            transform=emo_train_transfrom,
-            mode='train',
-        )
-        train_dataset.append(emotion_train_dataset)
-        train_dataset.extend(gaze_train_datasets)
-        for i, dataset in enumerate(train_dataset):
-            if i == 0:
-                weight = 10 / len(dataset)  # TODO: add a hyperparameter for this
-            else:
-                weight = 1 / len(dataset)
-            sampler_weights.extend([weight] * len(dataset))
-
-        train_dataset = ConcatDataset(train_dataset)
-
-
-        # Prepare val dataset
-        gaze_val_dataset = [
-            OnePersonDataset(path, gaze_val_transform)
-            for i, path in enumerate(train_paths)
-            if i in val_indices
-        ]
-        emotion_val_dataset = RafDataset(
-            raf_path=raf_dir,
-            transform=emo_val_transfrom,
-            mode='val',
-        )
-        val_dataset.append(emotion_val_dataset)
-        val_dataset.extend(gaze_val_dataset)
-        val_dataset = ConcatDataset(val_dataset)
-
-        # Initialize horizontal flip for all the datasets
-        if config.dataset.transform.train.horizontal_flip:
-            for dataset in train_dataset.datasets:
-                dataset.random_horizontal_flip = True
-
-        return train_dataset, val_dataset, sampler_weights
-
-    elif config.mode == GazeEstimationMethod.MPIIFaceGaze.name:
-        transform = create_transform(config)
-        assert config.train.test_id in range(-1, 15)
-        assert config.test.test_id in range(15)
-        ## index:02 practic iti pune cate 0 sa ai inainte de index = > p01 cand index=1
-        ## daca aveam index:03   ==> p001
-        person_ids = [f'p{index:02}' for index in range(15)]
-
-        if is_train:
-            # daca vren sa folosim tot dataset-ul pentru antrenare
-            if config.train.test_id == -1:
-                train_dataset = torch.utils.data.ConcatDataset([
-                    OnePersonDataset(person_id, dataset_dir, transform)
-                    for person_id in person_ids
-                ])
-                assert len(train_dataset) == 45000
-            # daca vrem sa pastram p00 pentru testare
-            else:
-                test_person_id = person_ids[config.train.test_id]
-                train_dataset = torch.utils.data.ConcatDataset([
-                    OnePersonDataset(person_id, dataset_dir, transform)
-                    for person_id in person_ids if person_id != test_person_id
-                ])
-                assert len(train_dataset) == 42000
-
-            # cat la suta din datele de training le folosim pentru testare
-            val_ratio = config.train.val_ratio
-            assert val_ratio < 1
-            val_num = int(len(train_dataset) * val_ratio)
-            train_num = len(train_dataset) - val_num
-            lengths = [train_num, val_num]
-            return torch.utils.data.dataset.random_split(train_dataset, lengths)
-        # daca suntem in faza de testare si cream un dataloader pentru evaluare
+    # Prepare train dataset
+    gaze_train_datasets = [
+        OnePersonDataset(path, gaze_train_transform)
+        for i, path in enumerate(train_paths)
+        if i not in val_indices
+    ]
+    emotion_train_dataset = RafDataset(
+        raf_path=raf_dir,
+        transform=emo_train_transfrom,
+        mode='train',
+    )
+    train_dataset.append(emotion_train_dataset)
+    train_dataset.extend(gaze_train_datasets)
+    for i, dataset in enumerate(train_dataset):
+        if i == 0:
+            weight = 10 / len(dataset)  # TODO: add a hyperparameter for this
         else:
-            test_person_id = person_ids[config.test.test_id]
-            test_dataset = OnePersonDataset(test_person_id, dataset_dir, transform)
-            assert len(test_dataset) == 3000
-            return test_dataset
+            weight = 1 / len(dataset)
+        sampler_weights.extend([weight] * len(dataset))
 
-    else:
-        raise ValueError('Bad dataset')
+    train_dataset = ConcatDataset(train_dataset)
+
+    # Prepare val dataset
+    gaze_val_dataset = [
+        OnePersonDataset(path, gaze_val_transform)
+        for i, path in enumerate(train_paths)
+        if i in val_indices
+    ]
+    emotion_val_dataset = RafDataset(
+        raf_path=raf_dir,
+        transform=emo_val_transfrom,
+        mode='val',
+    )
+    val_dataset.append(emotion_val_dataset)
+    val_dataset.extend(gaze_val_dataset)
+    val_dataset = ConcatDataset(val_dataset)
+
+    # Initialize horizontal flip for all the datasets
+    if config.dataset.transform.train.horizontal_flip:
+        for dataset in train_dataset.datasets:
+            dataset.random_horizontal_flip = True
+
+    return train_dataset, val_dataset, sampler_weights

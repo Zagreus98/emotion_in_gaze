cc1950d4951210b2ad34a2099e0dc364d52637bb
Zagreus98 <alex.ombla@gmail.com>
2023-05-31 00:52:19 +0300

    Added label_smoothing

--- gaze_estimation/losses.py
+++ gaze_estimation/losses.py
@@ -9,7 +9,8 @@ class TotalLoss(nn.Module):
     def __init__(self, config: yacs.config.CfgNode):
         super().__init__()
         self.config = config
-        self.emotion_loss = nn.CrossEntropyLoss(reduction="mean", ignore_index=-1)
+        # TODO: make label_smoothing cnfigurable
+        self.emotion_loss = nn.CrossEntropyLoss(reduction="mean", ignore_index=-1, label_smoothing=0.3)
         self.gaze_loss = self.create_gaze_loss()
 
     def create_gaze_loss(self) -> nn.Module:
